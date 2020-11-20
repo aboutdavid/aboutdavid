edc70f4dbf21d85b61f52e70640813c9a0d61ff6
aboutDavid <62346025+aboutDavid@users.noreply.github.com>
2020-11-20 11:17:18 -0500

    Update date.js

--- data/date.js
+++ data/date.js
@@ -1,16 +1,3 @@
-var obj = {}
-    var date = new Date();
-    var month = date.toLocaleString("default", { month: "long" });
-    var day = date.getDate();
-    var year = date.getFullYear();
-    var hour = date.getHours() + 1;
-    var min = date.getMinutes() + 1;
-       var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm
-obj.dateandtime = `${month} ${day}, ${year} (${strTime})`
-module.exports = obj
+var obj = {};
+obj.dateandtime = date.toLocaleString('en-US', { timeZone: 'EST', timeStyle: "short", dateStyle: "long" }).toString();
+module.exports = obj;
