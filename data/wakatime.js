44afd5d5d9bc9a39cede49df1d83d1bed6e083a4
David <62346025+aboutdavid@users.noreply.github.com>
2022-11-20 18:28:32 -0500

    Create wakatime.js

---
+++ data/wakatime.js
@@ -0,0 +1,20 @@
+var request = require("sync-request");
+var res = request(
+  "GET",
+  `https://wakatime.com/share/@aboutDavid/b8ce273b-e06b-41eb-a214-ea27ce6c8c67.json`,
+  {
+    headers: {
+      "user-agent": "David's Website"
+    }
+  }
+);
+var json = JSON.parse(res.getBody("utf8")).data
+json.sort((a,b)=>b.grand_total.total_seconds-a.grand_total.total_seconds)
+console.log(json)
+
+module.exports = {
+    highest: json[0].grand_total.text,
+    today: json.find(i=>i.range.date == `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`).grand_total.text,
+}
+
+console.log(module.exports)
