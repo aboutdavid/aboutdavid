cb9659c7463387d935b015b3c85e57a9f3603ee7
David <62346025+aboutdavid@users.noreply.github.com>
2022-11-20 19:54:59 -0500

    add checking

--- data/wakatime.js
+++ data/wakatime.js
@@ -11,10 +11,15 @@ var res = request(
 var json = JSON.parse(res.getBody("utf8")).data
 json.sort((a,b)=>b.grand_total.total_seconds-a.grand_total.total_seconds)
 console.log(json)
+var text
+var today = json.find(i=>i.range.date == `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`)
+if (!today) today = json.find(i=>i.range.date == `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()-1}`)
+if (today) text = today.grand_total.text
+if (!today) text = "Unable to fetch..."
 
 module.exports = {
     highest: json[0].grand_total.text,
-    today: json.find(i=>i.range.date == `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()}`).grand_total.text,
+    today: json.find(i=>i.range.date == `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()-1}`).grand_total.text,
 }
 
 console.log(module.exports)
