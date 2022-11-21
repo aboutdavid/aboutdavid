2df2e156688e00eecee8d5dd6093318ba19e7eca
David <62346025+aboutdavid@users.noreply.github.com>
2022-11-20 19:36:31 -0500

    update to est timezone

--- data/commit.js
+++ data/commit.js
@@ -10,20 +10,7 @@ var res = request(
   }
 );
 function time(date) {
-  var date = new Date(date);
-  var month = date.toLocaleString("default", { month: "long" });
-  var day = date.getDate();
-  var year = date.getFullYear();
-  var hour = date.getHours() + 1;
-  var min = date.getMinutes() + 1;
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? "pm" : "am";
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  var strTime = hours + ":" + minutes + " " + ampm;
-  return `${month} ${day}, ${year} (${strTime})`;
+return new Date(date).toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: "short", dateStyle: "long" }).toString()
 }
 const activities = JSON.parse(res.getBody());
 
