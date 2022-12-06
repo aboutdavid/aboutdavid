8ec99f84ac4ba52b1ff8956160fba69894db7598
David <62346025+aboutdavid@users.noreply.github.com>
2022-12-06 14:02:20 -0500

    remove redundancy

--- data/date.js
+++ data/date.js
@@ -1,4 +1,4 @@
-var obj = {};
 var date = new Date();
-obj.dateandtime = date.toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: "short", dateStyle: "long" }).toString();
-module.exports = obj;
+module.exports = {
+dateandtime: date.toLocaleString('en-US', { timeZone: 'America/New_York', timeStyle: "short", dateStyle: "long" })
+}
